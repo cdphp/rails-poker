335ee04bffecf8b1f0ef7ef9a7268ebe529bd981
jhuang <Joey.Huang@activenetwork.com>
2017-08-29 12:39:36 +0800

    production asset config

--- poker/config/environments/production.rb
+++ poker/config/environments/production.rb
@@ -17,13 +17,13 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  # config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.serve_static_assets = false
+  config.public_file_server.enabled = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
-  config.serve_static_assets = false
-  config.public_file_server.enabled = false
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
