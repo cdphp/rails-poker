c13a593c53ff59f8fdf59fe9afa3f82a61e0efc5
jhuang <Joey.Huang@activenetwork.com>
2017-09-04 18:11:50 +0800

    add modal for image

--- poker/app/assets/javascripts/components/chat_box.js.jsx
+++ poker/app/assets/javascripts/components/chat_box.js.jsx
@@ -1,11 +1,12 @@
 class ChatBox extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { chats: [] };
+    this.state = { chats: [], isModalOpen: false, modalContent: null};
   }
 
   componentDidMount() {
     this.loadChats();
+    this.bindModalImage();
     this.setupSubscription();
   }
 
@@ -14,6 +15,9 @@ class ChatBox extends React.Component {
       <div className="col-md-12 chat-container">
         <ChatList mename={this.props.me.name} chats={this.state.chats}/>
         <AddChat meid={this.props.me.id} onChatSubmit={this.handleChatSubmit.bind(this)}/>
+        <Modal show={this.state.isModalOpen} onClose={this.hideModal.bind(this)}>
+          {this.state.modalContent}
+        </Modal>
       </div>
     )
   }
@@ -32,6 +36,17 @@ class ChatBox extends React.Component {
     });
   }
 
+  bindModalImage(){
+    let r = this;
+    $("body").on('click', 'img', function(event) {
+      event.preventDefault();
+      /* Act on the event */
+      console.log("click");
+      r.renderImage(this.src)
+      r.showModal();
+    });
+  }
+
   handleChatSubmit(data) {
     App.channel.perform('speak', data);
   }
@@ -41,6 +56,26 @@ class ChatBox extends React.Component {
     var new_chat = JSON.parse(data);
     var new_chats_list = $.merge($.merge([], this.state.chats), new_chat.chats);
     this.setState({chats: new_chats_list.slice(-100)});
+  }
+
+  renderImage(imageUrl) {
+    this.setState({
+      modalContent: <img src={imageUrl} />
+    })
+  }
+  showModal(){
+    this.setState({
+      modalContent: this.state.modalContent,
+      isModalOpen: !this.state.isModalOpen
+    });
+    $("#myModal").modal('show');
+  }
+
+  hideModal(){
+    this.setState({
+      isModalOpen: !this.state.isModalOpen
+    });
+    $("#myModal").modal('hide');
   }
 
   setupSubscription(){
